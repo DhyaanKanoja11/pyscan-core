7428cadd93bf1687b92ee0f29f6c5ca0a54d1fd7
DhyaanKanoja11 <dhyaandk11@gmail.com>
2025-12-29 00:25:43 +0530

    add initial scanner flow

--- scanner.py
+++ scanner.py
@@ -1,1 +1,26 @@
+import sys
+from utils import resolve_dns, scan_ports, grab_banner
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python scanner.py <domain/IP>")
+        sys.exit(1)
+
+    target = sys.argv[1]
+
+    ip = resolve_dns(target)
+    if not ip:
+        print("Could not resolve target")
+        sys.exit(1)
+
+    open_ports = scan_ports(ip)
+    banners = grab_banner(ip, open_ports)
+
+    print("\nScan Results")
+    print("Target:", target)
+    print("IP:", ip)
+    print("Open Ports:", open_ports)
+    print("Banners:", banners)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,1 +1,8 @@
+def resolve_dns(target):
+    return None
 
+def scan_ports(ip):
+    return []
+
+def grab_banner(ip, ports):
+    return {}
